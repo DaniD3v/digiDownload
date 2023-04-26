e06cfc729c45ce9263620c65d3483639ce62713e
notyou <sch220233@spengergasse.at>
2023-04-26 19:21:22 +0200

    fixed dependencies 2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setuptools.setup(
     description="API to download books from digi4school.at.",
     keywords=["digi4school", "books", "api"],
 
-    version="1.0.4",
+    version="1.0.5",
     license='MIT',
 
     packages=["digiDownload"],
@@ -21,5 +21,5 @@ setuptools.setup(
         "beautifulsoup4"
     ],
 
-    download_url='https://github.com/DaniD3v/digiDownload/archive/refs/tags/v1.0.4.tar.gz',
+    download_url='https://github.com/DaniD3v/digiDownload/archive/refs/tags/v1.0.5.tar.gz',
 )
